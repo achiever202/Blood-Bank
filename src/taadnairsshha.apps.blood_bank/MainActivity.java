9b42c6ddde96a5b22177eaeda8dc9a1961df3d32
Adarsh Pugalia <es12b1001@iith.ac.in>
2014-04-01 01:57:42 +0530

    Update MainActivity.java
    Added a background image, and an onTap listener!

--- src/taadnairsshha.apps.blood_bank/MainActivity.java
+++ src/taadnairsshha.apps.blood_bank/MainActivity.java
@@ -6,20 +6,34 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		finish();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		Toast.makeText(getApplicationContext(), "Tap to Continue", Toast.LENGTH_SHORT).show();
+	}
+	
+	public void check(View view)
+	{
 		// creating a shared preference object.
 		SharedPreferences sharedPref = getSharedPreferences(getString(R.string.pref_file), Context.MODE_PRIVATE);
 		
 		// if the shared preference file exists, that is user is registered.
 		// forwarding to the home screen.
-		if(sharedPref.contains(getString(R.string.user_id)))
+		if(sharedPref.contains("Phone"))
 		{
 			Intent intent = new Intent(this, Home.class);
 			startActivity(intent);
